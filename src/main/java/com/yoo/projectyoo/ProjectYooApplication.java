d20219c42ccc2a1637a615cc9c9077b94249867e
KyeonghoYoo <ykh6242@etoos.com>
2022-09-29 13:28:42 +0900

    Initial commit

---
+++ src/main/java/com/yoo/projectyoo/ProjectYooApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.projectyoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectYooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectYooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoo/projectyoo/ProjectYooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.projectyoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectYooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
